45ffa5f3e3df64a8bee342b858b5f1cb3ee5ef4b
Titouan Bourlanges <titouan.bourlanges@epsi.fr>
2023-06-30 11:44:24 +0200

    skeleton + tests

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -3,7 +3,7 @@ var User = function (name) {
    this.name = name;
 };
 describe("Checking if the user is created correctly", function () {
-   it("should create the user with the correct name", function () {
+   it("shsdbvsbould create the user with the correct name", function () {
        debugger
        var tom = new User("tom");
        tom.name.should.be.equal("tom");
